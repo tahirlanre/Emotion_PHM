b4c424cde9cd1dfcf0c43fdc5f4c259097cc2c83
tahirlane <tahirlanre@hotmail.com>
2022-03-20 13:06:51 +0000

    Move train utility funcs to utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,14 @@
+import os
+import random
+import logging
+from pathlib import Path
+import shutil
 import logging
 from functools import wraps
+
+import torch
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 
 logger = logging.getLogger(__name__)
 
@@ -24,3 +33,56 @@ def log_step(func):
 
 def save_to_disk(dataf, path):
     dataf.to_csv(path, index=False)
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+
+def compute_metrics(labels, preds):
+    assert len(preds) == len(labels)
+    results = dict()
+
+    results["accuracy"] = accuracy_score(labels, preds)
+    (
+        results["macro_precision"],
+        results["macro_recall"],
+        results["macro_f1"],
+        _,
+    ) = precision_recall_fscore_support(labels, preds, average="macro")
+    (
+        results["micro_precision"],
+        results["micro_recall"],
+        results["micro_f1"],
+        _,
+    ) = precision_recall_fscore_support(labels, preds, average="micro")
+    (
+        results["weighted_precision"],
+        results["weighted_recall"],
+        results["weighted_f1"],
+        _,
+    ) = precision_recall_fscore_support(labels, preds, average="weighted")
+
+    return results
+
+
+def save_checkpoint(model_to_save, tokenizer, global_step, output_dir):
+    # delete older checkpoint(s)
+    glob_checkpoints = [str(x) for x in Path(output_dir).glob(f"checkpoint-*")]
+
+    for checkpoint in glob_checkpoints:
+        # logger.info(f"Deleting older checkpoint {checkpoint}")
+        shutil.rmtree(checkpoint)
+
+    # Save model checkpoint
+    ckpt_output_dir = os.path.join(output_dir, f"checkpoint-{global_step}")
+    os.makedirs(ckpt_output_dir, exist_ok=True)
+
+    # logger.info(f"Saving model checkpoint to {ckpt_output_dir}")
+
+    model_to_save.save_pretrained(ckpt_output_dir)
+    tokenizer.save_pretrained(ckpt_output_dir)
